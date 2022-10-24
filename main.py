48e0d4124d92c4a264fd899b35859d30c4e907ce
AliBushehri02 <hermiticuss@gmail.com>
2022-10-24 17:57:37 +0300

    f

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 print("Hello World") # yey :)
 print((1.00/9.81**0.5))
+print("The first answer is")
+
+#This is the First Root
+print((-2+(2**2-4*1*5**0.5)/2*1))
+
+#This is the Second Root
+print("This second answer is")
+print((-2+(2**2-4*1*5**0.5)/2*1))
+
+print("Goodbye")
